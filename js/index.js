18ff89cd8e8ae3a91fc7ac8073777f0f5d1b9ddd
phalado <phalado@gmail.com>
2020-02-20 13:40:41 -0300

    Created the skeleton for the project and a first version of a form to add the books

---
+++ js/index.js
@@ -0,0 +1,17 @@
+let myLibrary = []
+
+function Book(title, author, pages, status){
+  this.id = myLibrary.length;
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.status = status;
+};
+
+function addBookToLibrary(){
+  myLibrary.push(
+    new Book(
+      
+      )
+    )
+}
